d0b108bb89152a6236a7deb28256b30d5f8520a9
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-28 20:18:14 -0300

    do not download the source again and fix build_artifacts leak

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,10 @@
-if [ ! -e tzdb-$PKG_VERSION.tar.lz ]; then
-    wget https://data.iana.org/time-zones/releases/tzdb-${PKG_VERSION}.tar.lz
-fi
-lzip -d tzdb-$PKG_VERSION.tar.lz
+lzip -d tzdb-${PKG_VERSION}.tar.lz
+tar xvf tzdb-${PKG_VERSION}.tar --strip 1
 
-tar xvf tzdb-$PKG_VERSION.tar --strip 1
-
-mkdir dest
 make -e \
-  cc=$CC \
-  DESTDIR=dest \
-  TOPDIR=$PREFIX \
-  USRDIR=. \
+  cc=${CC} \
+  TOPDIR=${PREFIX} \
+  USRDIR="" \
   install
 
-prefix_num_dirs=$(echo $PREFIX | grep -o / | wc -l)
-tar cf - -C dest . | tar xf - -C $PREFIX --strip $(( prefix_num_dirs + 1 ))
-
-mv $PREFIX/sbin/zic $PREFIX/bin
+mv ${PREFIX}/sbin/zic ${PREFIX}/bin
