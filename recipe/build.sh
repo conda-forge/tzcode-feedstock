bdbc22fbbeb7074ed76ef12f948336b1bd939309
Uwe L. Korn <uwe.korn@quantco.com>
2020-12-16 20:35:36 +0100

    Add support for cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,26 @@
+set -exo pipefail
+
 lzip -d tzdb-${PKG_VERSION}.tar.lz
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  mkdir for_build
+  pushd for_build
+  tar xvf ../tzdb-${PKG_VERSION}.tar --strip 1
+  make -e \
+    CC=${CC_FOR_BUILD} \
+    TOPDIR=${BUILD_PREFIX} \
+    USRDIR="" \
+    install
+  mv ${BUILD_PREFIX}/sbin/zic ${BUILD_PREFIX}/bin
+  popd
+  MAKE_EXTRA_ARGS="zic=${BUILD_PREFIX}/bin/zic"
+fi
+
 tar xvf tzdb-${PKG_VERSION}.tar --strip 1
 
 make -e \
-  cc=${CC} \
-  TOPDIR=${PREFIX} \
+  CC=${CC} \
+  TOPDIR=${PREFIX} ${MAKE_EXTRA_ARGS:-} \
   USRDIR="" \
   install
 
