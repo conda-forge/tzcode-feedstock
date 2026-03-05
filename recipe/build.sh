3509c5a47d0abb80f39799e30f77593fe38b15af
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-03-05 10:24:06 +0100

    Add tzdata source so build succeeds
    However, make sure that none of the tzdata-related files actually
    get packaged.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,20 @@
 set -exo pipefail
 
 SOURCE_DIR="${PWD}"
+TZDATA_DIR="${PWD}/tzdata"
+
+# tzcode lacks tzdata inputs; bring in only missing files from the secondary
+# tzdata source so upstream make targets can run without file collisions.
+if [[ -d "${TZDATA_DIR}" ]]; then
+  shopt -s nullglob dotglob
+  for src in "${TZDATA_DIR}"/*; do
+    dst="${SOURCE_DIR}/$(basename "${src}")"
+    if [[ ! -e "${dst}" ]]; then
+      cp -a "${src}" "${dst}"
+    fi
+  done
+  shopt -u nullglob dotglob
+fi
 
 if [[ $(uname) == Darwin ]]; then
   CFLAGS="$CFLAGS -DHAVE_GETRANDOM=0"
@@ -10,27 +24,39 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
   mkdir for_build
   cp -a "${SOURCE_DIR}/." for_build/
   pushd for_build
+
+  # Keep data outputs out of the prefix while using upstream install logic.
+  BUILD_TZDIR="${PWD}/zoneinfo"
+  BUILD_TZDEFAULT="${PWD}/localtime"
+  BUILD_MANDIR="${PWD}/man"
+
   make -e \
     CC=${CC_FOR_BUILD} \
     TOPDIR=${BUILD_PREFIX} \
     USRDIR="" \
     ZICDIR="${BUILD_PREFIX}/bin" \
-    TZDIR="./share/zoneinfo" \
-    MANDIR="./man" \
-    TZDEFAULT="./etc/localtime" \
+    TZDIR="${BUILD_TZDIR}" \
+    MANDIR="${BUILD_MANDIR}" \
+    TZDEFAULT="${BUILD_TZDEFAULT}" \
     install
+
   popd
   MAKE_EXTRA_ARGS="zic=${BUILD_PREFIX}/bin/zic"
 fi
 
 cd "${SOURCE_DIR}"
 
+# Use upstream install, but keep data outputs outside PREFIX so this remains code-only.
+PKG_TZDIR="${PWD}/zoneinfo"
+PKG_TZDEFAULT="${PWD}/localtime"
+PKG_MANDIR="${PWD}/man"
+
 make -e \
   CC=${CC} \
   TOPDIR=${PREFIX} ${MAKE_EXTRA_ARGS:-} \
   USRDIR="" \
   ZICDIR="${PREFIX}/bin" \
-  TZDIR="./share/zoneinfo" \
-  MANDIR="./man" \
-  TZDEFAULT="./etc/localtime" \
+  TZDIR="${PKG_TZDIR}" \
+  MANDIR="${PKG_MANDIR}" \
+  TZDEFAULT="${PKG_TZDEFAULT}" \
   install
